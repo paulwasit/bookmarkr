20bffdec0fa344852c8f1c05ccd020dd4ba11a5b
paulwasit <plat.sebastien@hotmail.fr>
2016-06-19 20:05:47 +0200

    fix/simplify assets mgmt

--- config/assets/default.js
+++ config/assets/default.js
@@ -13,70 +13,11 @@ module.exports = {
     views:		'server/modules/*/views/*.html'
   },
 	client: {
-		lib: {css:[], js: "public/modules/articles/assets/highlightjs/highlight.pack.js"},
-		js: "public/dist/bundle.js",
-		css: "https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"
+		lib: {
+			css:"https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css", 
+			js: "public/modules/articles/assets/highlightjs/highlight.pack.js"
+		},
+		js: [],
+		css: []
 	}
-	
-	// , client: {
-    // lib: {
-      // css: [
-				//'public/lib/bootstrap/dist/css/bootstrap.css',
-				//'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-				// 'public/lib/font-awesome/css/font-awesome.min.css',
-				// 'public/lib/textAngular/dist/textAngular.css',
-				// 'public/lib/angular-ui-notification/dist/angular-ui-notification.css',
-				// 'public/lib/leaflet/dist/leaflet.css',
-				// 'public/lib/codemirror/lib/codemirror.css'
-      // ],
-      // js: [
-				// 'public/lib/jquery/dist/jquery.js',
-				// 'public/lib/jquery-ui/jquery-ui.js',
-				// 'public/lib/codemirror/lib/codemirror.js',
-				// 'public/lib/codemirror/mode/xml/xml.js',
-        // 'public/lib/angular/angular.js',
-        // 'public/lib/angular-resource/angular-resource.js',
-        // 'public/lib/angular-animate/angular-animate.js',
-        // 'public/lib/angular-messages/angular-messages.js',
-        // 'public/lib/angular-ui-router/release/angular-ui-router.js',
-        // 'public/lib/angular-ui-utils/ui-utils.js',
-        // 'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-        // 'public/lib/angular-file-upload/angular-file-upload.js',
-        // 'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-				// 'public/lib/angular-ui-notification/dist/angular-ui-notification.js',
-				// 'public/lib/moment.min.js',
-				// 'public/lib/leaflet/dist/leaflet.js',
-				// 'public/lib/angular-simple-logger/dist/angular-simple-logger.js',
-				// 'public/lib/ui-leaflet/dist/ui-leaflet.js',
-				// 'public/lib/textAngular/dist/textAngular-rangy.min.js',
-				// 'public/lib/textAngular/dist/textAngular-sanitize.min.js',
-				// 'public/lib/textAngular/dist/textAngular.min.js',
-				// 'public/lib/angular-ui-codemirror/ui-codemirror.js',
-				// 'public/lib/ngSticky/dist/sticky.min.js',
-				// 'public/lib/angular-ui-sortable/sortable.js'
-				
-      // ],
-      // tests: ['public/lib/angular-mocks/angular-mocks.js']
-    // },
-    // css: [
-      // 'public/modules/**/css/*.css'
-    // ],
-    // less: [
-      // 'public/modules/**/less/*.less'
-    // ],
-		// sassGlob: 'public/modules/_core/assets/core.scss',
-    // sass: [
-      // 'public/modules/**/scss/*.scss'
-    // ],
-    // js: [
-      // 'public/config.js',
-      // 'public/init.js',
-      // 'public/modules/*/*.module.js',
-			// 'public/modules/*/*.js',
-      // 'public/modules/*/!(tests)/**/*.js'
-    // ],
-    // views: ['public/modules/*/views/**/*.html'],
-    // templates: ['public/build/templates.js']
-  // }
-	
 };
--- config/assets/production.js
+++ config/assets/production.js
@@ -2,65 +2,7 @@
 
 module.exports = {
 	client: {
-		lib: {css:[], js: "public/modules/articles/assets/highlightjs/highlight.pack.js"},
 		js: "public/dist/bundle.min.js",
-		css: [
-			"public/dist/bundle.min.css",
-			"https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"
-		]
+		css: "public/dist/bundle.min.css"
 	}
-	/*
-  client: {
-    lib: {
-      css: [
-        //'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        //'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-				'public/lib/font-awesome/css/font-awesome.min.css',
-				'public/lib/textAngular/dist/textAngular.css',
-				'public/lib/angular-ui-notification/dist/angular-ui-notification.css',
-				'public/lib/leaflet/dist/leaflet.css',
-				'public/lib/codemirror/lib/codemirror.css',
-				'public/lib/ui-select/dist/select.min.css',
-				'public/lib/highlightjs/styles/default.css',
-				'public/dist/application.min.css'
-      ],
-      js: [
-			
-				'public/lib/jquery/dist/jquery.min.js',
-				'public/lib/jquery-ui/jquery-ui.min.js',
-				'public/lib/codemirror/lib/codemirror.js',
-				'public/lib/codemirror/mode/xml/xml.js',
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-ui-utils/ui-utils.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/angular-file-upload.min.js',
-        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-				'public/lib/angular-ui-notification/dist/angular-ui-notification.min.js',
-				'public/lib/moment.min.js',
-				'public/lib/leaflet/dist/leaflet.js',
-				'public/lib/angular-simple-logger/dist/angular-simple-logger.min.js',
-				'public/lib/ui-leaflet/dist/ui-leaflet.min.js',
-				'public/lib/textAngular/dist/textAngular-rangy.min.js',
-				'public/lib/textAngular/dist/textAngular-sanitize.min.js',
-				'public/lib/textAngular/dist/textAngularSetup.js',
-				'public/lib/textAngular/dist/textAngular.js',
-				'public/lib/angular-ui-codemirror/ui-codemirror.min.js',
-				'public/lib/ngSticky/dist/sticky.min.js',
-				'public/lib/angular-ui-sortable/sortable.min.js',
-				'public/lib/ui-select/dist/select.min.js',
-				'public/lib/highlightjs/highlight.pack.js',
-				'public/lib/marked/lib/marked.js',
-				'public/lib/angular-marked/dist/angular-marked.min.js',
-				'public/dist/application.min.js'
-      ]
-    },
-    //css: 'public/dist/application.min.css',
-    //js: 'public/dist/application.min.js'
-		
-  }
-	*/
 };
