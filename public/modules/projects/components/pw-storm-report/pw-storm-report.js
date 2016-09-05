c432ba072dfe47143803ad4c43b323201e641839
paulwasit <plat.sebastien@hotmail.fr>
2016-09-05 22:28:08 +0200

    improve db call speed by keeping only >0 records + new graphs

--- public/modules/projects/components/pw-storm-report/pw-storm-report.js
+++ public/modules/projects/components/pw-storm-report/pw-storm-report.js
@@ -6,9 +6,7 @@ module.exports = function (ngModule) {
 	
 	ngModule.component('pwStormReport', {
 		template: require('./pw-storm-report.html'),
-		bindings: {
-			jsonData: '='
-		},
+		bindings: {},
 		controller: ['StormReport', function (StormReport) {
 		
 			var ctrl = this, 
@@ -18,8 +16,11 @@ module.exports = function (ngModule) {
 			this.switchHarmType = switchHarmType;
 			this.updateMatchFields = updateMatchFields;
 			this.filterHarmEvents = filterHarmEvents;
+			this.buildChartDataObject = buildChartDataObject;
+			this.setAxisTicks = setAxisTicks;
+			
 			this.alert = function (msg) {
-				alert(msg);
+				console.log(ctrl.geoChart.data.rows[msg.row].c[0].v);
 			};
 			
 			// initialize exposed variables
@@ -36,7 +37,7 @@ module.exports = function (ngModule) {
 						floor: years[0],
 						ceil:  years[1],
 						onEnd: function() {
-							ctrl.updateMatchFields ("year");
+							ctrl.updateMatchFields ();
 						}
 					}
 				};
@@ -46,8 +47,7 @@ module.exports = function (ngModule) {
 					type: "GeoChart",
 					data: {
 						"cols": [
-							{id: "t", label: "State", type: "string"},
-							{id: "s", label: "Harm", type: "number"}
+							{id: "t", label: "Temp", type: "string"}
 						]
 					},
 					options: {
@@ -55,9 +55,55 @@ module.exports = function (ngModule) {
 						resolution: "provinces",
 						legend: {numberFormat:'short'},
 						colorAxis: {colors: ['#EFF3FF', '#084594']},
-						backgroundColor: "#f7f7f7",
+						//backgroundColor: "#f7f7f7",
 						//datalessRegionColor: '#f8bbd0',
 						defaultColor: '#f5f5f5'
+					}
+				};
+				
+				// states columns parameters
+				this.statesChart = {
+					type: "ColumnChart",
+					data: {
+						"cols": [
+							{id: "t", label: "Temp", type: "string"}
+						]
+					},
+					options: {
+						legend: { position: 'none' },
+						hAxis: {format:'short'},
+					}
+				};
+				
+				// years line chart
+				this.yearChart = {
+					type: "LineChart",
+					data: {
+						"cols": [
+							{id: "t", label: "Temp", type: "string"}
+						]
+					},
+					options: {
+						legend: { position: 'none' },
+						curveType: 'function',
+						vAxis: {format:'short'},
+						hAxis: {format: ''}
+					}
+				};
+				
+				// years line chart
+				this.eventTypesChart = {
+					type: "BarChart",
+					data: {
+						"cols": [
+							{id: "t", label: "Temp", type: "string"}
+						]
+					},
+					options: {
+						legend: { position: 'none' },
+						hAxis: {format:'short'},
+						//hAxis: {title: "Years" , direction:-1, slantedText:true, slantedTextAngle:90 }
+						chartArea: {width: '50%'} 
 					}
 				};
 				
@@ -66,8 +112,8 @@ module.exports = function (ngModule) {
 				this.harmEventTypes = [];
 				this.selectedHarmEventTypes = {};
 				this.matchFields = {};
-				this.geoChart.options.title = this.harmType + " Blabla";
 				this.switchHarmType();
+				this.geoChart.options.title = this.harmType + " Blabla";
 				
 			};
 			
@@ -77,12 +123,29 @@ module.exports = function (ngModule) {
 			// switch harm type: update events types list + refresh map
 			function switchHarmType () {
 				return StormReport.query({ harmTypeSwitch: ctrl.harmType }).$promise.then(function (result) {
-					ctrl.harmEventTypes = result;
+					// reset variables;
+					ctrl.harmEventTypes = [];
 					ctrl.selectedHarmEventTypes = {};
-					for (var i=0,l=ctrl.harmEventTypes.length;i<l;i++) {
-						ctrl.selectedHarmEventTypes[ctrl.harmEventTypes[i]._id]=true;
+					ctrl.matchFields = {};
+					// propDmg > 10000 to make the db call faster (we only lose 0.2% of the total)
+					if (ctrl.harmType === "propDmg") {
+						ctrl.matchFields[ctrl.harmType] = { $gt: 10000 };
 					}
-					ctrl.updateMatchFields("harmEventTypes");
+					else {
+						ctrl.matchFields[ctrl.harmType] = { $gt: 0 };
+					}
+					var harmEventType;
+					// loop over event types (other is pushed to the end of the array to appear last in the checkbox list
+					for (var i=0,l=result.length;i<l;i++) {
+						harmEventType = result[i];
+						if (harmEventType !== "OTHER") ctrl.harmEventTypes.push(harmEventType);
+						ctrl.selectedHarmEventTypes[harmEventType]=true;
+					}
+					ctrl.harmEventTypes.sort();
+					ctrl.harmEventTypes.push("OTHER");
+					console.log(ctrl.harmEventTypes);
+					// prepare & execute new db query
+					ctrl.updateMatchFields();
 				});
 			}
 			
@@ -90,61 +153,137 @@ module.exports = function (ngModule) {
 			
 			// update the filter of collection documents
 			function updateMatchFields (field) {
-				if (field == "year") {
-					// build new year filter
-					var year = {};
-					if (ctrl.yearSlider.min > years[0]) year["$gte"] = ctrl.yearSlider.min;
-					if (ctrl.yearSlider.max < years[1]) year["$lte"] = ctrl.yearSlider.max;
+				
+				// build new year filter
+				var year = {};
+				if (ctrl.yearSlider.min > years[0]) year["$gte"] = ctrl.yearSlider.min;
+				if (ctrl.yearSlider.max < years[1]) year["$lte"] = ctrl.yearSlider.max;
 					
-					// update matchFields if not the same value as before (angular.equals does not work with prop starting with $)
-					if (typeof ctrl.matchFields.year == "undefined" || year["$lte"] !== ctrl.matchFields.year["$lte"] || year["$gte"] !== ctrl.matchFields.year["$gte"]) {
-						if (Object.keys(year).length == 0) {
-							delete ctrl.matchFields.year;
-						}
-						else {
-							ctrl.matchFields.year = year;
-						}
-						ctrl.filterHarmEvents(ctrl.harmType, ctrl.matchFields);
+				// update matchFields if not the same value as before (angular.equals does not work with prop starting with $)
+				if (typeof ctrl.matchFields.year == "undefined" || year["$lte"] !== ctrl.matchFields.year["$lte"] || year["$gte"] !== ctrl.matchFields.year["$gte"]) {
+					if (Object.keys(year).length == 0) {
+						delete ctrl.matchFields.year;
 					}
-				}
-				else if (field == "harmEventTypes") {
-					// push selected event types in an array
-					var eventTypes = [],
-							eventField = ctrl.harmType + "Top10";
-					for (var i=0,l=ctrl.harmEventTypes.length;i<l;i++) {
-						if (ctrl.selectedHarmEventTypes[ctrl.harmEventTypes[i]._id]==true) eventTypes.push(ctrl.harmEventTypes[i]._id);
-					}
-					// delete filter if all event types are selected
-					if (eventTypes.length === 10) {
-						delete ctrl.matchFields[eventField];
-					}
-					// add filter field otherwise
 					else {
-						ctrl.matchFields[eventField] = { $in: eventTypes };
+						ctrl.matchFields.year = year;
 					}
-					ctrl.filterHarmEvents(ctrl.harmType, ctrl.matchFields);
 				}
+				
+				// push selected event types in an array
+				var eventTypes = [],
+						eventField = ctrl.harmType + "Top10";
+				for (var i=0,l=ctrl.harmEventTypes.length;i<l;i++) {
+					if (ctrl.selectedHarmEventTypes[ctrl.harmEventTypes[i]]==true) eventTypes.push(ctrl.harmEventTypes[i]);
+				}
+				// delete filter if all event types are selected
+				if (eventTypes.length === 10) {
+					delete ctrl.matchFields[eventField];
+				}
+				// add filter field otherwise
+				else {
+					ctrl.matchFields[eventField] = { $in: eventTypes };
+				}
+				
+				ctrl.filterHarmEvents(ctrl.harmType, ctrl.matchFields);
+				
 			}
 			
 			// get results & format them for the geochart
 			function filterHarmEvents (harmType, matchFields) {
+				
+				// format query
 				var matchFields = (Object.keys(matchFields).length != 0) ? JSON.stringify(matchFields) : undefined,
-					harmType = JSON.stringify("$" + harmType);
+						harmType = JSON.stringify("$" + harmType);
 
-				return StormReport.query({ fields: matchFields, harmType: harmType }).$promise.then(function (result) {
-					result[0] = JSON.parse(result[0]);
-					var newRow, rows = [];
-					for (var i=0,l=result[0].length; i < l ; i++) {
-						newRow = {
-							c: [
-								{v: result[0][i].stateName},
-								{v: result[0][i].totalHarm}
-							]
-						};
-						rows.push(newRow)
+				// execute query
+				return StormReport.get({ fields: matchFields, harmType: harmType }).$promise.then(function (result) {
+
+					// build map data
+					var data = buildChartDataObject(result['statesMap'], { _id: ['State', 'string'] , totalHarm: [ctrl.harmType, 'number'] });
+					ctrl.geoChart.data = data;
+					
+					// build states data
+					ctrl.statesChart.data = data;
+					
+					// build years data
+					var data = buildChartDataObject(result['years'], { _id: ['Year', 'number'] , totalHarm: [ctrl.harmType, 'number'] });
+					ctrl.yearChart.data = data;
+					
+					// adapt years only when active years filter (to show the whole period otherwise)
+					if (typeof matchFields !== "undefined" && typeof JSON.parse(matchFields).year !== "undefined") {
+						ctrl.yearChart.options.hAxis.ticks = ctrl.setAxisTicks(result['years'], '_id', 10);
 					}
-					ctrl.geoChart.data.rows = rows;
+					else {
+						ctrl.yearChart.options.hAxis.ticks = ctrl.setAxisTicks('[{"_id":' + years[0] + '},{"_id":' + years[1] + '}]', '_id', 10);
+					}
+					
+					// build years data
+					var data = buildChartDataObject(result['eventTypes'], { _id: [ctrl.harmType + "Top10", 'string'] , totalHarm: [ctrl.harmType, 'number'] });
+					ctrl.eventTypesChart.data = data;
+					
 				});
+			}
+			
+			// build chart data object
+			function buildChartDataObject (source, headers) {
+				
+				source = JSON.parse(source);
+				var data = {
+					cols: [],
+					rows: []
+				};
+				
+				// column headers
+				var newCol, header, colNames = [], colId = 1;
+				for (var key in headers) {
+					if (!headers.hasOwnProperty(key)) continue;
+					header  = headers[key];
+					if (header !== 0) {
+						newCol = {
+							id: colId,
+							label: header[0],
+							type:  header[1]
+						};
+						data.cols.push(newCol);
+						colNames.push(key);
+						colId ++;
+					}
+				}
+				
+				// rows
+				var newRow, newRowCol;
+				for (var i=0,l=source.length; i < l ; i++) {
+					newRow = { c: [] };
+					for (var j=0,k=colNames.length; j < k ; j++) {
+						newRowCol = { v: source[i][colNames[j]] }
+						newRow.c.push(newRowCol);
+					}
+					data.rows.push(newRow);
+				}
+				
+				return data;
+				
+			}
+			
+			function setAxisTicks (source, variable, step) {
+				source = JSON.parse(source);
+				var ticks = [], minValue = Infinity, maxValue = -Infinity, tmpValue;
+				
+				for (var i=0,l=source.length; i < l ; i++) {
+					tmpValue = source[i][variable];
+					if (tmpValue < minValue) minValue = tmpValue;
+					if (tmpValue > maxValue) maxValue = tmpValue;
+				}
+				
+				var minTick = Math.ceil(minValue/step),
+						maxTick = Math.floor(maxValue/step);
+						
+				for (i=minTick; i <= maxTick ; i++) {
+					ticks.push(i*step);
+				}
+				
+				return ticks;
+				
 			}
 			
 		}]
--- server/modules/stormReport/controllers/stormReport.server.controller.js
+++ server/modules/stormReport/controllers/stormReport.server.controller.js
@@ -13,24 +13,78 @@ var mongoose = require('mongoose'),
  */
 exports.list = function (req, res) {
 	
-	var aggregateFieldsA = [],
-			aggregateFieldsB = [],
-			aggregateFieldsC = [],
-			queryResults = [];
-		
-	// aggregation query: add match filter if relevant
-	if (typeof req.query.fields != "undefined") { 
-		aggregateFieldsA.push({ $match: JSON.parse(req.query.fields) });	
-		aggregateFieldsB.push({ $match: JSON.parse(req.query.fields) });	
-		aggregateFieldsC.push({ $match: JSON.parse(req.query.fields) });	
+	var harmType = JSON.parse(req.query.harmType),
+			query = [],
+			queryResults = {};
+	
+	// group by state
+	addQuery (
+		"statesMap",
+		{ $group: { _id: "$State", stateName: { $first: "$stateName"}, totalHarm: { $sum: harmType } } },
+		{ $sort:  { totalHarm: -1 } }
+	);
+
+	// group by year
+	addQuery (
+		"years",
+		{ $group: { _id: "$year", totalHarm: { $sum: harmType } } },
+		{ $sort:  { _id: 1 } }
+	);
+	
+	// group by event types
+	addQuery (
+		"eventTypes",
+		{ $group: { _id: harmType + "Top10", totalHarm: { $sum: harmType } } },
+		{ $sort:  { totalHarm: -1 } }
+	);
+	
+	
+	// ---------- add match + run queries ---------- //
+	
+	// add match filter if needed
+	if (typeof req.query.fields != "undefined") {
+		var matchFields = JSON.parse(req.query.fields);
+		for (var i=0, l=query.length; i<l; i++) { query[i].query.unshift({ $match: matchFields }); }
+	}
+
+	// run all queries
+	var countdown = query.length,
+			isError = false, errMsg = "";
+	
+	for (var i=0, l=query.length; i<l; i++) { 
+		StormReport.aggregate(query[i].query, callback(query[i].chartName));
 	}
 	
-	// aggregation query: add grouping
-	aggregateFieldsA.push({ $group: { _id: "$State", stateName: { $first: "$stateName"}, totalHarm: { $sum: JSON.parse(req.query.harmType) } } });
 	
-	// track callbacks of all aggregate functions;
-	var countdown = 1;
-	var isDone = function () {
+	// ---------- internal functions ---------- //
+	
+	// add query
+	function addQuery () {
+		var newQuery = {
+			chartName: arguments[0],
+			query: []
+		};
+		for (var i = 1; i < arguments.length; i++) {
+			newQuery.query.push(arguments[i]);
+		}
+		query.push(newQuery);
+	}
+	
+	// callback after mongodb resolve
+	function callback (propertyName) {
+		return function (err, results) {
+			if (err) {
+				isError = true;
+				errMsg = errMsg + '\n' + errorHandler.getErrorMessage(err);
+			} else {
+				queryResults[propertyName] = (JSON.stringify(results));
+			}
+			isDone();
+		};
+	}
+	
+	// return the results object once every callback is done
+	function isDone () {
 		countdown -=1;
 		// all done, call exit
 		if (countdown == 0) {
@@ -39,28 +93,33 @@ exports.list = function (req, res) {
 		};
 	}
 	
-	
-	// execute aggregate queries
-	var isError = false, errMsg = "";
-	var queryA = StormReport.aggregate(aggregateFieldsA);
-  queryA.exec(function (err, results) {
-    if (err) {
-			isError = true;
-			errMsg = errMsg + '\n' + errorHandler.getErrorMessage(err);
-    } else {
-      queryResults.push(JSON.stringify(results));
-    }
-		isDone();
-  });
-	
 };
 
 exports.harmTypeSwitch = function (req, res) {
 	
-	var aggregateFields = [{ $group: { _id: "$" + req.params.harmTypeSwitch + "Top10" } }];
+	var top10;
+	
+	switch(req.params.harmTypeSwitch) {
+		case "cropDmg":
+			top10 = ["DROUGHT", "EXTREME COLD/WIND CHILL", "FLASH FLOOD", "FLOOD", "FROST/FREEZE", "HAIL", "HURRICANE/TYPHOON", "ICE STORM", "THUNDERSTORM WIND", "OTHER"];
+			break;
+		case "propDmg":
+			top10 = ["FLASH FLOOD", "FLOOD", "HAIL", "HURRICANE/TYPHOON", "STORM SURGE/TIDE", "THUNDERSTORM WIND", "TORNADO", "TROPICAL STORM", "WILDFIRE", "OTHER"];
+			break;
+		case "Injuries":
+			top10 = ["EXCESSIVE HEAT", "FLASH FLOOD", "FLOOD", "HEAT", "ICE STORM", "LIGHTNING", "THUNDERSTORM WIND", "TORNADO", "WILDFIRE", "OTHER"];
+			break;
+		default:
+			top10 = ["EXCESSIVE HEAT", "EXTREME COLD/WIND CHILL", "FLASH FLOOD", "FLOOD", "HEAT", "LIGHTNING", "RIP CURRENT", "THUNDERSTORM WIND", "TORNADO", "OTHER"];
+	}
+	
+	res.json(top10);
 	
 	// execute aggregate query
-	var query = StormReport.aggregate(aggregateFields);
+	//var query = StormReport.aggregate(aggregateFields);
+	
+	/*
+	var query = StormReport.distinct(req.params.harmTypeSwitch + "Top10");
   query.exec(function (err, results) {
     if (err) {
       return res.status(400).send({
@@ -70,7 +129,7 @@ exports.harmTypeSwitch = function (req, res) {
       res.json(results);
     }
   });
-	
+	*/
 };
 	
 	
