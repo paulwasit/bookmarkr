7d8a25b7bd60ea7c4fa6474e72dccd25d69dd060
paulwasit <plat.sebastien@hotmail.fr>
2016-09-22 22:05:58 +0200

    add contact module - frontend

--- public/modules/contact/pw-contact.js
+++ public/modules/contact/pw-contact.js
@@ -2,34 +2,52 @@
 
 module.exports = function (ngModule) {
 	
+	require('~/users/helpers/show-errors')(ngModule); 
+	
 	ngModule.component('pwContact', {
 		template: require('./pw-contact.html'),
 		bindings: {},
-		controller: [function () {
+		controller: ["$rootScope", "$location", "$http", "$timeout", function ($rootScope, $location, $http, $timeout) {
 		
 			var ctrl = this;
 			
 			// exposed functions
-			//this.toggleAsideCollapsed = toggleAsideCollapsed;
+			this.submitAction = submitAction;
 			
 			// initialize exposed variables
-			this.$onInit = function () {};
+			this.$onInit = function () {
+				this.contact = {};
+				this.error = $location.search().err;
+			};
 			
 			
 			// ---------- private: functions declaration ---------- //
 			
-			//function toggleAsideCollapsed () {}
-			
-			
-			// broadcast
-			/*
-			$rootScope.$on('cfpLoadingBar:started', function($event, $element, $target){
-				ctrl.isLoading = true;
-			});
-			$rootScope.$on('cfpLoadingBar:completed', function($event, $element, $target){
-				ctrl.isLoading = false;
-			});
-			*/
+			function submitAction (isValid) {
+				
+				ctrl.success = ctrl.error = null;
+				if (!isValid) {
+					$rootScope.$broadcast('show-errors-check-validity', '$ctrl.userForm');
+					return false;
+				}
+				
+				ctrl.pending = true;
+				
+				$http.post('/api/contact/sendEmail', ctrl.contact)
+				.then(function(response) {
+					ctrl.pending = false;
+					ctrl.contact = {};
+					ctrl.userForm.$setPristine();
+					ctrl.success = response.data.message;  // Show user success message
+				})
+				.catch(function(response) {
+					console.log('error: ', response);
+					ctrl.pending = false;
+					ctrl.userform = null;					    // Clear form and clear form
+					ctrl.error = response.data.message;    // Show user error message
+				});
+				
+			}
 			
 		}]
 		
