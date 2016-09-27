22dfcebf59dd098b3c90b7fbe5e141e7a5c8cf50
paulwasit <plat.sebastien@hotmail.fr>
2016-09-27 17:47:40 +0200

    try to fix smatKeyboard focus on iOS

--- public/modules/projects/components/pw-smart-keyboard/pw-smart-keyboard.js
+++ public/modules/projects/components/pw-smart-keyboard/pw-smart-keyboard.js
@@ -47,31 +47,81 @@ module.exports = function (ngModule) {
 			function onInputEvent (eventType) {
 				
 				var updateValue;
-				ctrl.check += eventType + "/ ";
+				//ctrl.check += eventType + "// ";
 				
 				// inProg prevents flicker of suggested words update on android after pressing the internal keyboard buttons 
-				// (word + space on change, then space removal with keyup, then readdition of space with keyup)
-				if (ctrl.inProg) return;
-				if (eventType === "keydown") {
-					ctrl.cursorPos = ctrl.textArea[0].selectionStart
-					return;
+				// (word + space on change, then space removal with keyup, then readdition of space with keyup - only when not modifying an existing word)
+				if (ctrl.inProg === true) return;
+				
+				// android: click on built-in keyboard
+				// ctrl.check += eventType + "-" + ctrl.cursorPos + "-" + ctrl.textArea[0].selectionStart + "/ ";
+				if (eventType === "change" && ctrl.os === "Android" && ctrl.cursorPos < ctrl.textArea[0].selectionStart) {
+					ctrl.check += "newWordAndroid";
+					updateValue = ctrl.textArea.val() + " ";
+					updatePos = ctrl.textArea[0].selectionStart + 1;
+					ctrl.inProg = true;
+					ctrl.cursorPos = ctrl.textArea[0].selectionStart;
+					$timeout(function() {
+						ctrl.inProg = false;
+					},100);
+				}
+				// iOS: click on built-in keyboard
+				else if (eventType === "change" && ctrl.os === "iOS" && ctrl.lastEvent !== "down") {
+					ctrl.check += "newWordIOS";
+					updateValue = ctrl.textArea.val() + " ";
+					updatePos = ctrl.textArea[0].selectionStart + 1;
+					ctrl.inProg = true;
+					ctrl.cursorPos = ctrl.textArea[0].selectionStart;
+					$timeout(function() {
+						ctrl.inProg = false;
+					},100);
+				}
+				
+				/*
+				if (eventType === "down") {
+					
+					console.log("");
+					
+					//ctrl.cursorPos = ctrl.textArea[0].selectionStart
+					//ctrl.inProg = "down";
+					//$timeout(function() {
+					//	ctrl.inProg = false;
+					//	return;
+					//},10);
 				}
 				
 				ctrl.check += "ok/ ";
 				ctrl.check += ctrl.cursorPos + "-" + ctrl.textArea[0].selectionStart + "/ ";
+				*/
+				
+				/*
+				if (eventType === "change" && ctrl.inProg === "down") {
+					if (ctrl.cursorPos < ctrl.textArea[0].selectionStart) {
+						
+					}
+					else {
+						ctrl.check += "CAncel/ ";
+						ctrl.inProg = false;
+						return;
+					}
+				}
+				ctrl.inProg = false;
+				*/
 				
 				// action when words update using the internal keyboard (Windows Phone, iOS & Android only)
+				/*
 				if (eventType === "change" && ctrl.cursorPos < ctrl.textArea[0].selectionStart && ctrl.os !== "unknown") {
 					ctrl.check += "fck/ ";
 					updateValue = ctrl.textArea.val() + " ";
 					updatePos = ctrl.textArea[0].selectionStart + 1;
-					/*
+					
 					if (ctrl.os === "iOS") {
 						ctrl.textArea.val(updateValue);
 						ctrl.cursorPos = updatePos;
 					}
 					else {
-						*/
+						
+					//if (ctrl.inProg === "down") {
 						ctrl.inProg = true;
 						ctrl.cursorPos = ctrl.textArea[0].selectionStart;
 						$timeout(function() {
@@ -79,11 +129,14 @@ module.exports = function (ngModule) {
 						},100);
 					//}
 				}
+				*/
 				else {
 					updateValue = ctrl.textArea.val();
 					updatePos = ctrl.textArea[0].selectionStart;
 					ctrl.cursorPos = ctrl.textArea[0].selectionStart;
 				}
+				
+				ctrl.lastEvent = eventType;
 				
 				// feels hack-ish, but ngKeyup doesn't update the model when spacebar or return are pressed - also accounts for button presses
 				if (ctrl.inputText !== ctrl.textArea.val()) ctrl.inputText = ctrl.textArea.val(); 
